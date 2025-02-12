fixing standard deviation calculations

ML/src/fileio.py
@@ -154,6 +154,7 @@ def las2split(infile_pcs,
         if veg_indices == 'rgb' or veg_indices is None:
             indat = veg_rgb(indat)
         else:
+            print('\nGeometry metrics specified: {}'.format(geometry_metrics))
             indat = vegidx(indat, 
                            indices=veg_indices, 
                            geom_metrics=geometry_metrics)
@@ -168,7 +169,7 @@ def las2split(infile_pcs,
         indat['veglab'] = np.full(shape=len(indat), 
                                   fill_value=class_val, 
                                   dtype=np.float32)
-        print(indat['veglab'])
+        # print(indat['veglab'])
 
         # # transpose the data
         # globals()[os.path.splitext(os.path.basename(ifile))[0]+'_dat'] = np.transpose(globals()[os.path.splitext(os.path.basename(ifile))[0]+'_dat'])
@@ -276,7 +277,7 @@ def df_to_dataset(dataframe,
     dataframe = dataframe.copy()
     if shuffle:
         dataframe = dataframe.sample(frac=1).reset_index(drop=True)
-        if verbose > 0:
+        if verbose == 2:
             print(dataframe.head())
     if not targetcolname == 'none':
         # assume that the last column is the labels
@@ -297,14 +298,14 @@ def df_to_dataset(dataframe,
         ds_inputs = tf.convert_to_tensor(dataframe)
         ds = tf.data.Dataset.from_tensor_slices((ds_inputs, labels))
 
-        if verbose > 0:
+        if verbose == 2:
             print(ds)
             print(ds.element_spec[0].shape[1:])
     else:
         ds_inputs = tf.convert_to_tensor(dataframe)
         ds = tf.data.Dataset.from_tensor_slices((ds_inputs))
 
-        if verbose > 0:
+        if verbose == 2:
             print(ds)
 
     # if shuffle:
@@ -332,8 +333,10 @@ def generate_dataframe(input_point_cloud, vegetation_index_list, dtype_conversio
         outdict['x'] = np.array(input_point_cloud.x, dtype=dtype_conversion).flatten()
         outdict['y'] = np.array(input_point_cloud.y, dtype=dtype_conversion).flatten()
         outdict['z'] = np.array(input_point_cloud.z, dtype=dtype_conversion).flatten()
-    if any('sd' in m for m in vegetation_index_list):
+    if any('3d' in m for m in vegetation_index_list):
         outdict['sd3d'] = np.array(input_point_cloud.sd3d, dtype=dtype_conversion).flatten()
+    if any('sd' in m for m in vegetation_index_list):
+        outdict['sd_x'] = np.array(input_point_cloud.sd_x, dtype=dtype_conversion).flatten()
     if any('exg' == m for m in vegetation_index_list) or any('simple' in m for m in vegetation_index_list) or any('all' in m for m in vegetation_index_list):
         outdict['exg'] = np.array(input_point_cloud.exg, dtype=dtype_conversion).flatten()
     if any('exr' in m for m in vegetation_index_list) or any('simple' in m for m in vegetation_index_list) or any('all' in m for m in vegetation_index_list):
@@ -357,7 +360,7 @@ def generate_dataframe(input_point_cloud, vegetation_index_list, dtype_conversio
     
     return pd.DataFrame(outdict)
 
-def predict_reclass_write(incloudname, model_list, threshold_vals, batch_sz=32, ds_cache=False, indiceslist=[], geo_metrics=[], geom_rad=0.10, verbose_output=2):
+def predict_reclass_write(incloudname, model_list, threshold_vals, batch_sz=32, ds_cache=False, indiceslist=[], geo_metrics=[], geom_rad=0.10, verbose_output=1):
     '''
     Reclassify the input point cloud using the models specified in the model_list
     variable and the threshold value(s) specified in the threshold_vals list. It
@@ -409,11 +412,16 @@ def predict_reclass_write(incloudname, model_list, threshold_vals, batch_sz=32, 
     modnamelist = [str(f.name) for f in model_list]
     print('List of models for reclassification: {}'.format(modnamelist))
 
+    if not isinstance(indiceslist, list):
+        indiceslist = list(indiceslist)
+
     # figure out what vegetation indices to compute
     if any('xyz' in m for m in modnamelist):
         indiceslist.append('xyz')
+    if any('3d' in m for m in modnamelist):
+        indiceslist.append('3d')
     if any('sd' in m for m in modnamelist):
-        indiceslist.append('sd3d')
+        indiceslist.append('sd')
     if any('all' in m for m in modnamelist):
         indiceslist = 'all'
     if any('simple' in m for m in modnamelist):
@@ -458,8 +466,8 @@ def predict_reclass_write(incloudname, model_list, threshold_vals, batch_sz=32, 
                                       cache_ds=ds_cache, 
                                       batch_size=batch_sz,
                                       drop_remain=False)
-    print(converted_dataset)
-    print(len(converted_dataset))
+    # print(converted_dataset)
+    # print(len(converted_dataset))
 
     del(indat)
     odir,ofname = os.path.split(incloudname)
@@ -476,13 +484,15 @@ def predict_reclass_write(incloudname, model_list, threshold_vals, batch_sz=32, 
         print('Reclassifying using {} model'.format(m.name))
         outdat_pred = m.predict(converted_dataset, verbose=2, use_multiprocessing=True)
 
-        print('\nOutput Predictions (raw): {}'.format(len(outdat_pred)))
-        print(outdat_pred)
+        if verbose_output == 2:
+            print('\nOutput Predictions (raw): {}'.format(len(outdat_pred)))
+            print(outdat_pred)
 
         outdat_pred = tf.argmax(outdat_pred,-1)
 
-        print('\nOutput Predictions: {}'.format(len(outdat_pred)))
-        print(outdat_pred)
+        if verbose_output == 2:
+            print('\nOutput Predictions: {}'.format(len(outdat_pred)))
+            print(outdat_pred)
 
         # print('threshold_vals = {}'.format(threshold_vals))
         if not isinstance(threshold_vals, list):
@@ -561,10 +571,10 @@ def predict_reclass_write(incloudname, model_list, threshold_vals, batch_sz=32, 
                                 ('results_' + rdate + '/' + ofname + "_" + str(m.name) + "_" + str(threshold_vals).replace('.','') + '.las')])
             elif int(laspy.__version__.split('.')[0]) == 2:
                 # update the classification values
-                print('\nincloud.classification: {}'.format(len(incloud.classification)))
-                print(incloud.classification)
-                print('\nOutput Predictions: {}'.format(len(outdat_pred_reclass)))
-                print(outdat_pred_reclass)
+                # print('\nincloud.classification: {}'.format(len(incloud.classification)))
+                # print(incloud.classification)
+                # print('\nOutput Predictions: {}'.format(len(outdat_pred_reclass)))
+                # print(outdat_pred_reclass)
                 incloud.classification = outdat_pred_reclass
                 # write out the new file (with vegetation indices included as extra bytes, is specified)
                 print('Writing LAZ file: {}'.format(os.path.join(odir,ofname+"_"+str(m.name)+"_"+str(threshold_vals).replace('.','')+'.copc.laz')))

ML/src/miscfx.py
@@ -4,6 +4,7 @@ import pandas as pd
 import laspy
 import matplotlib.pyplot as plt
 from scipy.spatial import cKDTree
+from tqdm import tqdm
 
 # import machine learning libraries
 import tensorflow as tf
@@ -405,25 +406,47 @@ def calc_3d_sd(coords, rad=0.5):
     # return the numpy array of computed standard deviation values all points in the cloud
     return sd
 
-def calc_3d_sd2(coords, rad=0.5):
+def calc_sd(coords, rad=0.5):
     # build the KDTree
     tree = cKDTree(coords, leafsize=5)
     # intiialize an empty numpy array of the same length as coords
-    sd = np.zeros((len(coords), 3))
+    sd = np.zeros(shape=(len(coords),3), dtype=float)
     # iterate over every point in the dense point cloud
     # (I think this is where the real slowdown is happening)
-    # perform spatial query on point
-    results = tree.query_ball_point(coords, r=rad)
-    for count, result in enumerate(results):
+    for count,elem in enumerate(tqdm(coords)):
+        # perform spatial query on point
+        result = tree.query_ball_point(elem, r=rad)
         # if at least one other point is returned, then continue
         if len(result) > 1:
-            # compute standard deviation
-            sd[count] = np.std(coords[result], axis=0)
+            # compute standard deviation of X, Y, and Z separately
+            sd[count] = np.std(coords[result],axis=0)
         # otherwise, no points were found in the search radius, return zero
         else:
             sd[count] = 0
     # return the numpy array of computed standard deviation values all points in the cloud
-    return np.sqrt(np.sum(sd ** 2, axis=1))
+    a,b,c = np.split(sd,3,axis=1)
+    return a.flatten(),b.flatten(),c.flatten()
+
+''' NOT SURE IF THIS NEXT FUNCTION WORKS '''
+# def calc_3d_sd2(coords, rad=0.5):
+#     # build the KDTree
+#     tree = cKDTree(coords, leafsize=5)
+#     # intiialize an empty numpy array of the same length as coords
+#     sd = np.zeros((len(coords), 3))
+#     # iterate over every point in the dense point cloud
+#     # (I think this is where the real slowdown is happening)
+#     # perform spatial query on point
+#     results = tree.query_ball_point(coords, r=rad)
+#     for count, result in enumerate(results):
+#         # if at least one other point is returned, then continue
+#         if len(result) > 1:
+#             # compute standard deviation
+#             sd[count] = np.std(coords[result], axis=0)
+#         # otherwise, no points were found in the search radius, return zero
+#         else:
+#             sd[count] = 0
+#     # return the numpy array of computed standard deviation values all points in the cloud
+#     return np.sqrt(np.sum(sd ** 2, axis=1))
 
 
 '''

ML/src/vegindex.py
@@ -1,9 +1,10 @@
 import sys
 import time
 import numpy as np
+from sklearn import preprocessing as skpre
 from .miscfx import *
 
-def vegidx(lasfileobj, indices=['rgb'], geom_metrics=[], colordepth=8, geom_radius=0.10):
+def vegidx(lasfileobj, indices=['rgb'], geom_metrics=[], colordepth=8, geom_radius=1.00):
     '''
     Compute specified vegetation indices and/or geometric values.
 
@@ -146,7 +147,37 @@ def vegidx(lasfileobj, indices=['rgb'], geom_metrics=[], colordepth=8, geom_radi
     # if standard deviation is specified as a geometric metric, then
     # compute the sd using a pointwise approach
     # NOTE: This computation is very time and resource expensive.
-    if ('sd' in geom_metrics) or ('sd' in indices):
+    if any('sd' in m for m in geom_metrics) or any('sd' in m for m in indices):
+        print('    Calculating standard deviation in X, Y, and Z')
+        # compute standard deviations
+        starttime = time.time()
+        if not 'sd_x' in dim_names:
+            lasfileobj.add_extra_dim(laspy.ExtraBytesParams(
+                name="sd_x",
+                type=np.float32,
+                description="standard_deviation_x_direction"
+                ))
+        if not 'sd_y' in dim_names:
+            lasfileobj.add_extra_dim(laspy.ExtraBytesParams(
+                name="sd_y",
+                type=np.float32,
+                description="standard_deviation_y_direction"
+                ))
+        if not 'sd_z' in dim_names:
+            lasfileobj.add_extra_dim(laspy.ExtraBytesParams(
+                name="sd_z",
+                type=np.float32,
+                description="standard_deviation_z_direction"
+                ))
+        lasfileobj.sd_x,lasfileobj.sd_y,lasfileobj.sd_z = calc_sd(np.array([lasfileobj.x, lasfileobj.y, lasfileobj.z]).transpose(), rad=geom_radius)
+        # # normalize the values
+        # mmscaler = skpre.MinMaxScaler()
+        # lasfileobj.sd_x = mmscaler.fit_transform(lasfileobj.sd_x)
+        # lasfileobj.sd_y = mmscaler.fit_transform(lasfileobj.sd_y)
+        # lasfileobj.sd_z = mmscaler.fit_transform(lasfileobj.sd_z)
+        print("Time to compute X,Y,Z Standard Deviations = {}".format(time.time()-starttime))
+    if any('3d' in m for m in geom_metrics) or any('3d' in m for m in indices):
+        print('    Calculating 3D standard deviation')
         # compute 3D standard deviation
         starttime = time.time()
         if not 'sd3d' in dim_names: